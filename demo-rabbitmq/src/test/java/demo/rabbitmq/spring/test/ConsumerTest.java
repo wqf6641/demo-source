faec34ffe8e546797c50eb87600dd658c1bb91e2
wengqf <wqf6641@yeah.net>
2018-09-29 18:46:40 +0800

    spring-rabbitmq

--- demo-rabbitmq/src/test/java/demo/rabbitmq/spring/test/ConsumerTest.java
+++ demo-rabbitmq/src/test/java/demo/rabbitmq/spring/test/ConsumerTest.java
@@ -3,6 +3,11 @@ package demo.rabbitmq.spring.test;
 import org.springframework.beans.BeansException;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
+/**
+ * 注意amqp-client与spring-rabbit版本兼容问题
+ * @author wengqf
+ *
+ */
 public class ConsumerTest {
 
 	public static void main(String[] args) throws Exception {
